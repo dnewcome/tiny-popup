bb0a225e9156bd537152977b3f7b7b67c512d55a
Dan Newcome <dnewcome@elevenprime.com>
2012-06-15 03:06:25 -0700

    added ability to specify new popup contents

--- tinypopup.js
+++ tinypopup.js
@@ -1,88 +1,95 @@
-// constructor
-// id - dom element id for popup
-var Tinypopup = ( function() {
-function applyStyles( el, obj ) {
-	for( style in obj ) {
-		if( style == 'top' || style == 'left' || style == 'width' || style == 'height' ) {
-			el.style[style] = obj[style] + 'px';
-		}
-		else {
-			el.style[style] = obj[style];
-		}
+var Tinypopup = (function(window) {
+
+	/* 
+	* constructor function 
+	*	id - dom element id for popup
+	*/
+	function tinypopup( id ) {
+		var self = this;
+		var el = document.getElementById( id );		
+		this.el = el;
+
+		applyStyles( el, { 
+			backgroundColor: 'white', position: 'fixed', display: 'none', zIndex: 1000 
+		} );
+
+		addShadow.apply(this);
+
+		this.shadow.addEventListener( 'click', function() { self.hide(); }, false );
+		window.addEventListener( 'resize', function() { resize.apply(self); }, false );
 	}
-}
 
-function tinypopup( id ) {
-	var self = this;
-	var el = document.getElementById( id );		
-	this.el = el;
+	/**
+	* show the popup, centered in the viewable client area
+	* with given width/height
+	*/
+	tinypopup.prototype.show = function( w, h, content ) {
+		this.w = w;
+		this.h = h;
+		if( content ) {
+			this.el.innerHTML = '';
+			this.el.appendChild( content );
+		}
+		this.el.style.display = 'block';
+		this.shadow.style.display = 'block';
+		resize.apply(this);	
+	}
 
-	applyStyles( el, { 
-		backgroundColor: 'white', position: 'fixed', display: 'none' 
-	} );
+	/**
+	* hide the popup
+	*/
+	tinypopup.prototype.hide = function() {
+		this.el.style.display = 'none';
+		this.shadow.style.display = 'none';
+	}
 
-	addShadow.apply(this);
-
-	this.shadow.addEventListener( 'click', function() { self.hide(); }, false );
-	window.addEventListener( 'resize', function() { self.onResize(); }, false );
-}
-
-	tinypopup.prototype.onResize = function() {
+	/**
+	* internal method for calculating/recalculating sizes of
+	* popup window and its shadow
+	*/
+	function resize() {
 		var cw = window.innerWidth, 
 			ch = window.innerHeight;
-
 		applyStyles( this.shadow, { 
 			width: cw, height: ch 
 		} );
-
 		applyStyles( this.el, { 
 			top: (ch-this.h)/2, left: (cw-this.w)/2, width: this.w, height: this.h 
 		} );
 	}
 
+	/**
+	* construct the shadow
+	*/
 	function addShadow() {
-		console.log('adding  shadow');
-		var cw = window.innerWidth, 
-			ch = window.innerHeight;
+		var shadow = this.shadow = document.createElement( 'div' );
+		this.el.parentNode.appendChild( shadow );
+		shadow.id = 'tinypopup-shadow';	
 
-		var shadow = document.createElement('div');
-		this.shadow = shadow;
-		this.el.parentNode.appendChild(shadow);
-		shadow.id = 'shadow';	
-
+		var zIndex = parseInt( this.el.style.zIndex ) - 1;
 		applyStyles( shadow, {
-			backgroundColor: 'grey', position: 'fixed', width: cw, height: ch, top: 0, left: 0, display: 'none' 
+			backgroundColor: 'grey', position: 'fixed', top: 0, left: 0, 
+			display: 'none', opacity: 0.5, zIndex: zIndex
 		} );
 
 	}
 
-
-	tinypopup.prototype.show = function( w, h ) {
-		this.w = w;
-		this.h = h;
-		var el = this.el;
-		el.style.display = 'block';
-		this.onResize();	
-/*
-		var cw = window.innerWidth, ch = window.innerHeight;
-		var left = (cw-w)/2;
-		var top = (ch-h)/2;
-		el.style.top = top + 'px';
-		el.style.left = left + 'px';
-		el.style.width = w + 'px';
-		el.style.height = h + 'px';
-*/
-
-		this.shadow.style.display = 'block';
-		this.shadow.style.opacity = 0.5;
-		this.el.style.zIndex = '1000';
-
-		this.shadow.style.zIndex = parseInt( this.el.style.zIndex ) - 1;
+	/**
+ 	* utility for applying multiple css styles to an element
+ 	* allows assignment of of dimensions using numeric values
+ 	*/
+	function applyStyles( el, obj ) {
+		for( style in obj ) {
+			if( style == 'top' || style == 'left' || 
+				style == 'width' || style == 'height' ) {
+				el.style[style] = obj[style] + 'px';
+			}
+			else {
+				el.style[style] = obj[style];
+			}
+		}
 	}
 
-	tinypopup.prototype.hide = function() {
-		this.el.style.display = 'none';
-		this.shadow.style.display = 'none';
-	}
+	// return the constructor function from module
 	return tinypopup;
-})();
+})(window);
